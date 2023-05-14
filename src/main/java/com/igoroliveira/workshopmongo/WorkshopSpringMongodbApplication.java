e456008886a813952b6451f56308cd48c7bcdb09
Igor <igoroliveira789@gmail.com>
2023-05-14 01:22:02 -0300

    Project Created

---
+++ src/main/java/com/igoroliveira/workshopmongo/WorkshopSpringMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.igoroliveira.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igoroliveira/workshopmongo/WorkshopSpringMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igoroliveira.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
