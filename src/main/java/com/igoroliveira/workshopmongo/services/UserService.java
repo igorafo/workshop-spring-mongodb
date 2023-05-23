e0fc96ca28c80dd850ad322baf6458cb55064b75
Igor <igoroliveira789@gmail.com>
2023-05-23 12:07:02 -0300

    inserting users with post, deleting users with delete and updating users with put

--- src/main/java/com/igoroliveira/workshopmongo/services/UserService.java
+++ src/main/java/com/igoroliveira/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.igoroliveira.workshopmongo.domain.User;
+import com.igoroliveira.workshopmongo.dto.UserDTO;
 import com.igoroliveira.workshopmongo.repository.UserRepository;
 import com.igoroliveira.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,29 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Ojbeto Não encontrado"));
+	}
+	
+	public User insert(User user) {
+		return repo.insert(user);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
+	public User update(User user) {
+		User newUser = findById(user.getId());
+		updateData(newUser, user);
+		return repo.save(newUser);
+	}
+	
+	private void updateData(User newUser, User user) {
+		newUser.setName(user.getName());
+		newUser.setEmail(user.getEmail());		
+	}
+
+	public User fromDTO(UserDTO userDto) {
+		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
 	}
 }
