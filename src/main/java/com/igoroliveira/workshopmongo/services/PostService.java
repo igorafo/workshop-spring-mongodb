7dd72d187ecce86e081078f08c268a013bf76b98
Igor <igoroliveira789@gmail.com>
2023-05-26 11:29:23 -0300

    obtaining posts by id

---
+++ src/main/java/com/igoroliveira/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.igoroliveira.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.igoroliveira.workshopmongo.domain.Post;
+import com.igoroliveira.workshopmongo.repository.PostRepository;
+import com.igoroliveira.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> post = repo.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Ojbeto Não encontrado"));
+	}
+}
